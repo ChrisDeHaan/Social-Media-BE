44f6f5fe89368c0f8420bfc8822954ab4a9c8cdd
Chris DeHaan <christopherwdehaan@gmail.com>
2023-11-10 21:16:32 -0500

    data seeding function works and has script

--- utils/seed.js
+++ utils/seed.js
@@ -10,23 +10,50 @@ const {
 connection.on('error', err => err)
 
 connection.once('open', async () => {
-    let  userCheck = await connection.db.listCollections({ name: 'users' }).toArray()
+    let userCheck = await connection.db.listCollections({ name: 'users' }).toArray()
     if (userCheck.length) {
         await connection.dropCollection('users')
     }
 
-    let thoughtCheck = await connection.db.listCollections({name: 'thoughts' }).toArray()
+    let thoughtCheck = await connection.db.listCollections({ name: 'thoughts' }).toArray()
     if (thoughtCheck.length) {
         await connection.dropCollection('thoughts')
     }
 
-    // Populate Users
-    const usernames = []
-
-    // Populate Thoughts
+    const users = []
     const thoughts = []
 
-    //Populate Reactions
-    const reactions = []
+    for (let i = 0; i < 20; i++) {
+        const username = getRandomUsername()
+        const email = getRandomEmail()
 
+        users.push({
+            username,
+            email
+        })
+
+        const reactions = []
+        for (let i=0; i < Math.floor(Math.random() * 4 + 1); i++) {
+            const rndmReaction = getRandomReaction()
+            reactions.push(rndmReaction)
+        }
+
+        const thoughtText = []
+        for (let i=0; i < Math.floor(Math.random() * 4 + 1); i++) {
+            const rndmThought = getRandomThought()
+            thoughtText.push(rndmThought)
+        }
+
+        thoughts.push({
+            thoughtText,
+            username,
+            reactions
+        })
+    }
+
+    await User.collection.insertMany(users);
+    await Thought.collection.insertMany(thoughts);
+
+    console.log('Data has been seeded.')
+    process.exit(0)
 })
